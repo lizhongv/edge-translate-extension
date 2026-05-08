c57af0adb129f01fd081be351ff54848564d79d8
zhongli2 <zhongli2@iflytek.com>
2026-05-08 12:34:22 +0800

    fix(manifest): content_scripts.all_frames=true (inject into iframes)

--- src/manifest.ts
+++ src/manifest.ts
@@ -24,6 +24,7 @@ export default defineManifest({
             matches: ["<all_urls>"],
             js: ["src/content/index.ts"],
             run_at: "document_idle",
+            all_frames: true,
         },
     ],
     side_panel: {
